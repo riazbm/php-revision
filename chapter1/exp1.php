b8026f14d01e43f793691da840fea9e2e8634e7f
riziki <rizikimohamed968@gmail.com>
2024-03-20 17:02:53 +0300

    outputs ways complete

---
+++ chapter1/exp1.php
@@ -0,0 +1,12 @@
+<!-- Displayong outputs -->
+
+<?php 
+  echo "junior software developer<br>";
+  echo 'PHP is fun and easy!<br>';
+  print "Always reach out to @Riazbm for help";
+  echo "<br>Today is Friday, we're going to the zoo";
+  //use of backslashes too
+  echo '<br>Today is Friday, we\'re going to the zoo';
+  echo "\"";   
+  echo "\\";
+
