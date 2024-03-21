807c592ff91645616cbc718b91384407c94fb124
riziki <rizikimohamed968@gmail.com>
2024-03-21 11:20:35 +0300

    final touches for variables and contants

--- chapter2/exp2.php
+++ chapter2/exp2.php
@@ -1,6 +1,7 @@
  <?php 
    define('basic_no', 12345);
    echo basic_no;
+   echo "<br>";
 
 //    variables and assigning them values
    $name = "Riaz";
@@ -10,7 +11,7 @@
    $state = "Karnataka";
    $country = "India";
    $x = 5;
-   echo 'the value is $x';
+   echo "the value is $x ";
    echo '<br>';
    echo "the age is $age";
 
