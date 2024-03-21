4eb66cd5547a40106b7253ef27fec36e4545d167
riziki <rizikimohamed968@gmail.com>
2024-03-21 11:18:14 +0300

    variables complete

--- chapter2/exp2.php
+++ chapter2/exp2.php
@@ -1,4 +1,17 @@
  <?php 
    define('basic_no', 12345);
    echo basic_no;
+
+//    variables and assigning them values
+   $name = "Riaz";
+   $age = 23;
+   $address = "Bangalore";
+   $city = "Bangalore";
+   $state = "Karnataka";
+   $country = "India";
+   $x = 5;
+   echo 'the value is $x';
+   echo '<br>';
+   echo "the age is $age";
+
  ?>  
