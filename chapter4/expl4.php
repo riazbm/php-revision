cb8e5ce47e373388e9358aab922f68ee326e297f
riziki <rizikimohamed968@gmail.com>
2024-03-22 12:23:54 +0300

    sample if statement

--- chapter4/expl4.php
+++ chapter4/expl4.php
@@ -16,3 +16,21 @@ and(&&)
 or(||)
 not(!!)
 
+<!-- Control structures in PHP -->
+
+<?php 
+//   if statement
+  
+  $a = 7;
+   if ($a<0)
+   {
+     echo 'if block<br>';
+     echo '$a is smaller than 0';
+   } 
+   else if ($a < 5)
+   {
+     'first elseif block';
+   }
+   else 'false block';
+
+?>
