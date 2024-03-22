4d12f8705d7056333e9f961ccc6502e2da48727d
riziki <rizikimohamed968@gmail.com>
2024-03-22 18:21:19 +0300

    function declaration done

---
+++ chapter5/exp5.php
@@ -0,0 +1,24 @@
+<?php
+  function displayGreetings(){
+    echo "hello";
+    echo '<br>';
+  }
+
+  function displayGreetings2($name, $message){
+    echo "Hello $name, $message";
+    echo '<br>';
+  }
+  displayGreetings2('Dlscar', 'good morning');
+
+  function displayGreeting3($name, $message = 'have a good day'){
+    echo "Hello $name, $message";
+    echo '<br>';
+  }
+  displayGreeting3('Dlscar');
+  displayGreeting3('Dlscar', 'how are you?');
+
+  function addNumbers($num1, $num2, $num3){
+    return $num1 + $num2 + $num3;
+     
+  }
+?>
