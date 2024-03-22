fd217d312deaaf4009478a0f87480a08e3903e3e
riziki <rizikimohamed968@gmail.com>
2024-03-22 12:14:38 +0300

    operators where they return bolean based on the statement

---
+++ chapter4/expl4.php
@@ -0,0 +1,18 @@
+<!-- comparison operators -->
+
+equal(==)
+identical(===)
+not equal(!= or <>)
+not identical(!==)
+less than(<)
+greater than(>)
+less than or equal to(<=)
+greater than or equal to(>=)
+spaceship(<=>)
+
+<!-- Logical Operators -->
+
+and(&&)
+or(||)
+not(!!)
+
