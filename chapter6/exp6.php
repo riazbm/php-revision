9ecb7f6c5aee88a088a9bbb9870cd5f3e42e6132
riziki <rizikimohamed968@gmail.com>
2024-03-23 16:15:56 +0300

    Html form complete

---
+++ chapter6/exp6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP form handling</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="text" name="studentname" value="your name" placeholder="Enter name"><br><br>
+        Favourite subject<br>
+        <input type="checkbox" name="sub[]" value="EL">English <br>
+        <input type="checkbox" name="sub[]" value="MA"Math <br>
+        <input type="checkbox" name="sub[]" value="PG">Programming<br><br>
+        Gender<br>
+        <input type="radio" name="gender" value="M">Male<br>
+        <input type="radio" name="gender" value="M">Female<br><br>
+        <input type="submit" name="sm" value="submit form">
+    </form>
+</body>
+</html>
