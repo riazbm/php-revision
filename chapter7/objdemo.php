4b453d516ce4dd37c778d3ad1cbe8bce5932f46a
riziki <rizikimohamed968@gmail.com>
2024-03-25 18:57:56 +0300

    accessing class members ie plus constants

--- chapter7/objdemo.php
+++ chapter7/objdemo.php
@@ -2,3 +2,10 @@
   include './movie.php';
   $mov1 = new movie('N0001', 'Lusso', 4.9);
   $mov2 = new movie('P0002', 'Junior', 5.99);
+
+  echo $mov1->title.'<br>';
+  echo $mov2->conversion('Japan').'<br>';
+
+  echo movie::DISCOUNT.'<br>';
+  echo $mov1::DISCOUNT.'<br>';
+  echo $mov2::DISCOUNT.'<br>';
