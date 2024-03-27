bbef11bf3a317a4b87560c3e93847f5a39bf7032
riziki <rizikimohamed968@gmail.com>
2024-03-27 14:30:38 +0300

    SQL injection sample done

---
+++ chapter9/expl9.php
@@ -0,0 +1,11 @@
+<?php
+  
+  $pdo = new PDO("mysql:host=localhost;dbname=pawszone", "pz_admin", "ABCD");
+
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $owner = $_POST['ownerName'];
+
+  $sql = "SELECT * FROM pets WHERE owner = '$owner'";
+  $stmt = $pdo->query($sql);
+
