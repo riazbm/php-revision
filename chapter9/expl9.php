32b6c188152e3ae2dc8393382741ebf10bd36039
riziki <rizikimohamed968@gmail.com>
2024-03-27 15:12:07 +0300

    owner name updated

--- chapter9/expl9.php
+++ chapter9/expl9.php
@@ -4,7 +4,7 @@
 
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $owner = $_POST['ownerName'];
+  $owner = $_POST['ownername'];
 
   $sql = "SELECT * FROM pets WHERE owner = '$owner'";
   $stmt = $pdo->query($sql);
